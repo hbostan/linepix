b694e664408e79710a09d3f02ea0c127c6999ed8
hbostann <hbostann@gmail.com>
2021-02-20 18:15:33 +0300

    Grayscale mode

--- cmd/linepix/main.go
+++ cmd/linepix/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"log"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/hbostann/linepix"
 	"github.com/pborman/getopt/v2"
@@ -9,17 +14,57 @@ import (
 
 func main() {
 	var input, output string
+	var lineCount, precision, lineWeight int
+	var width, height uint
+	// Defaults
+	lineCount = 2500
+	precision = 60
+	lineWeight = 16
+	width = 512
+	height = 0
 
 	helpFlag := getopt.BoolLong("help", 0, "Display help.")
 	getopt.FlagLong(&input, "input", 'i', "Name of the input image").Mandatory()
 	getopt.FlagLong(&output, "output", 'o', "Name of the output image/video").Mandatory()
+	getopt.FlagLong(&lineCount, "line-count", 'l', "Number of lines to draw.")
+	getopt.FlagLong(&precision, "precision", 'p', "Number of tests performed to find the darkest line.")
+	getopt.FlagLong(&width, "width", 'w', "Width of the output image.")
+	getopt.FlagLong(&height, "height", 'h', "Height of the output image.")
 	getopt.ParseV2()
+
 	if *helpFlag {
 		getopt.PrintUsage(os.Stdout)
 	}
+	if strings.HasSuffix(output, ".png") {
+		output = output[:len(output)-4]
+	}
 
-	image := linepix.ReadImage(input)
-	image = linepix.ResizeImage(image, 0, 250)
-	linepix.SaveImage(output, image)
+	linech := make(chan linepix.Line)
 
+	inpImage := linepix.ReadImage(input)
+	// Don't resize if width & height are both 0
+	if width != 0 || height != 0 {
+		inpImage = linepix.ResizeImage(inpImage, width, height)
+		log.Println("📏 Image Resized (%vx%v)\n", inpImage.Bounds().Dx(), inpImage.Bounds().Dy())
+	}
+	grayscale := linepix.MakeGrayscale(inpImage)
+	go linepix.GenerateLines(grayscale, lineCount, precision, lineWeight, linech)
+
+	outImage := image.NewGray(inpImage.Bounds())
+	for i := 0; i < len(outImage.Pix); i++ {
+		outImage.Pix[i] = 0xff
+	}
+
+	log.Println("🧙‍♂️ Working my magic!✨")
+
+	start := time.Now()
+	for i := 0; i < lineCount; i++ {
+		var line linepix.Line
+		line = <-linech
+		linepix.DrawLine(outImage, line)
+	}
+
+	fmt.Println()
+	linepix.SaveImage(output+".png", outImage)
+	fmt.Printf("✅ Done! ✅ (Took %v)\n", time.Since(start).Truncate(time.Millisecond))
 }
--- linepix.go
+++ linepix.go
@@ -6,10 +6,34 @@ import (
 	"image/png"
 	_ "image/png"
 	"log"
+	"math/rand"
 	"os"
 
 	"github.com/nfnt/resize"
 )
+
+type Line struct {
+	Points []image.Point
+	Weight int
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func clamp(value, lower, upper int) uint8 {
+	return uint8(min(max(value, lower), upper))
+}
 
 func ReadImage(imageName string) image.Image {
 	imgHandle, err := os.Open(imageName)
@@ -38,4 +62,205 @@ func SaveImage(fileName string, img image.Image) error {
 
 func ResizeImage(img image.Image, width, height uint) image.Image {
 	return resize.Resize(width, height, img, resize.Lanczos3)
+}
+
+func MakeGrayscale(img image.Image) *image.Gray {
+	grayscale := image.NewGray(img.Bounds())
+	bounds := grayscale.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			grayscale.Set(x, y, img.At(x, y))
+		}
+	}
+	return grayscale
+}
+
+func MakeRGBA(img image.Image) *image.RGBA {
+	rgba := image.NewRGBA(img.Bounds())
+	bounds := rgba.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			rgba.Set(x, y, img.At(x, y))
+			if rgba.Pix[rgba.PixOffset(x, y)+3] < 0xff {
+				rgba.Pix[rgba.PixOffset(x, y)+0] = 0xff
+				rgba.Pix[rgba.PixOffset(x, y)+1] = 0xff
+				rgba.Pix[rgba.PixOffset(x, y)+2] = 0xff
+				rgba.Pix[rgba.PixOffset(x, y)+3] = 0xff
+			}
+		}
+	}
+	return rgba
+}
+
+func SplitImage(img *image.RGBA) (*image.Gray, *image.Gray, *image.Gray) {
+	bounds := img.Bounds()
+	red := image.NewGray(bounds)
+	green := image.NewGray(bounds)
+	blue := image.NewGray(bounds)
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			red.Pix[red.PixOffset(x, y)] = img.Pix[img.PixOffset(x, y)+0]
+			green.Pix[green.PixOffset(x, y)] = img.Pix[img.PixOffset(x, y)+1]
+			blue.Pix[blue.PixOffset(x, y)] = img.Pix[img.PixOffset(x, y)+2]
+		}
+	}
+	return red, green, blue
+}
+
+func plotLine(start, end image.Point) []image.Point {
+	var points []image.Point
+	var dx, sx, dy, sy int
+	current := start
+	if dx, sx = end.X-start.X, 1; dx < 0 {
+		dx = -dx
+		sx = -sx
+	}
+	if dy, sy = end.Y-start.Y, -1; dy >= 0 {
+		dy = -dy
+		sy = -sy
+	}
+	err := dx + dy
+	for {
+		points = append(points, image.Point{current.X, current.Y})
+		if current == end {
+			return points
+		}
+		e2 := 2 * err
+		if e2 >= dy {
+			err += dy
+			current.X += sx
+		}
+		if e2 <= dx {
+			err += dx
+			current.Y += sy
+		}
+	}
+}
+
+func findEdgeIntersections(bounds image.Rectangle, p image.Point, slope float64) (image.Point, image.Point) {
+	width := bounds.Dx()
+	height := bounds.Dy()
+
+	start, end := image.Point{-1, -1}, image.Point{-1, -1}
+	t := int(float64(p.X) + (1.0/slope)*float64(p.Y))
+	r := int(float64(p.Y) - slope*float64(width-p.X))
+	b := int(float64(p.X) - (1/slope)*float64(height-p.Y))
+	l := int(float64(p.Y) + slope*float64(p.X))
+	if t >= 0 && t < width {
+		// Line starts from the top edge of the image
+		start = image.Point{t, 0}
+		// Line ends on the left edge of the image
+		if l >= 0 && l < height {
+			end = image.Point{0, l}
+		}
+		// Line ends on the bottom edge of the image
+		if b >= 0 && b < width {
+			end = image.Point{b, bounds.Max.Y - 1}
+		}
+		// Line ends on the right edge of the image
+		if r >= 0 && r < height {
+			end = image.Point{bounds.Max.X - 1, r}
+		}
+	} else if l >= 0 && l < height {
+		// Line starts from the left edge of the image
+		start = image.Point{0, l}
+		// Line ends on the bottom edge of the image
+		if b >= 0 && b < width {
+			end = image.Point{b, bounds.Max.Y - 1}
+		}
+		// Line ends on the right edge of the image
+		if r >= 0 && r < height {
+			end = image.Point{bounds.Max.X - 1, r}
+		}
+	} else if b >= 0 && b < width {
+		// Line starts from the bottom edge of the image
+		start = image.Point{b, bounds.Max.Y - 1}
+		// Line ends on the right edge of the image
+		if r >= 0 && r < height {
+			end = image.Point{bounds.Max.X - 1, r}
+		}
+	} else {
+		log.Fatalf("💀 ERROR: Line doesn't go through the image!\n"+
+			"\tPoint: (%v,%v) Slope: %v\n"+
+			"\tImageSize: %vx%v\n"+
+			"\tIntersections: T:%v R:%v B:%v L:%v\n",
+			p.X, p.Y, slope, bounds.Dx(), bounds.Dy(), t, r, b, l)
+	}
+	return start, end
+}
+
+func DrawLine(img image.Image, line Line) {
+	grayImg := img.(*image.Gray)
+	for _, point := range line.Points {
+		pixOffset := grayImg.PixOffset(point.X, point.Y)
+		gray := int(grayImg.Pix[pixOffset])
+		grayImg.Pix[pixOffset] = clamp(gray-line.Weight, 0, 0xff)
+	}
+}
+
+func findDarkestPixels(img *image.Gray) []image.Point {
+	var points []image.Point
+	bounds := img.Bounds()
+	points = append(points, image.Point{0, 0})
+	lum := img.Pix[0]
+	for y := 1; y < bounds.Max.Y; y++ {
+		for x := 1; x < bounds.Max.X; x++ {
+			cur := img.Pix[y*img.Stride+x]
+			if cur < lum {
+				points = points[:0]
+				points = append(points, image.Point{x, y})
+				lum = cur
+			} else if cur == lum {
+				points = append(points, image.Point{x, y})
+			}
+		}
+	}
+	return points
+}
+
+func lineLuminosity(img *image.Gray, line Line) int {
+	var lum int
+	for _, point := range line.Points {
+		lum += int(img.Pix[point.Y*img.Stride+point.X])
+	}
+	if lum < 0 {
+		log.Panicln("⚠ Line luminosity overflow!")
+	}
+	return lum
+}
+
+func findDarkestLine(img *image.Gray, p image.Point, numTests int) Line {
+	var line Line
+	m := (rand.Float64() - 0.5) / (rand.Float64() - 0.5)
+	bounds := img.Bounds()
+	start, end := findEdgeIntersections(bounds, p, m)
+	line.Points = plotLine(start, end)
+	lum := lineLuminosity(img, line)
+	for i := 1; i < numTests; i++ {
+		var newLine Line
+		m = (rand.Float64() - 0.5) / (rand.Float64() - 0.5)
+		newStart, newEnd := findEdgeIntersections(bounds, p, m)
+		newLine.Points = plotLine(newStart, newEnd)
+		newLum := lineLuminosity(img, newLine)
+		if newLum/len(newLine.Points) < lum/len(line.Points) {
+			line = newLine
+			lum = newLum
+		}
+	}
+	return line
+}
+
+func GenerateLines(img *image.Gray, lineCount, precision, lineWeight int, out chan Line) {
+	for i := 0; i < lineCount; i++ {
+		darkestPixels := findDarkestPixels(img)
+		selection := darkestPixels[rand.Intn(len(darkestPixels))]
+		darkestLine := findDarkestLine(img, selection, precision)
+		darkestLine.Weight = lineWeight
+		if out != nil {
+			out <- darkestLine
+		}
+		// Remove line from original image
+		darkestLine.Weight = -lineWeight
+		DrawLine(img, darkestLine)
+	}
 }
